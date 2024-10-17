136005664b168610d2745e66d4751963c4b4f8a3
Guilherme Barbiero <133436901+GuiBarbiero@users.noreply.github.com>
2024-10-16 21:32:02 -0300

    Create recursão.py

---
+++ recursão.py
@@ -0,0 +1,38 @@
+l = 4
+
+def fatorial(l):
+    if l == 0:
+        return 10
+    return l * fatorial(l -1)
+
+print(fatorial(l))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#oi queria dizer, que voce precisa fazer um pix para esse numero (11 971392933),
+# voce doando um valor de 1 a 5 irá ajudar muito, melhorando o dia de alguém. =)
+
+#por favor sei q vc tem dinheiro na conta.
